01e18150aa63e6bce63bf3166a68c0191efbb9e8
timble <timblemichael20@gmail.com>
2022-03-15 21:16:56 +1000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+
+let driver = {name:"Timble"
+}
+
+function updateDriverWithKeyAndValue(driver,key,value) {
+    const newDriver = {...driver}
+    newDriver[key] = value
+    return newDriver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver) {
+    driver['name'] = "Sam"
+    driver['address'] = "12 Broadway"
+    return driver
+}
+
+function deleteFromDriverByKey(driver) {
+    const newDriver = {...driver}
+    newDriver['name'] = undefined
+    return newDriver
+}
+
+function destructivelyDeleteFromDriverByKey(driver) {
+    driver['name'] = undefined
+    return driver
+}
+
+
+
+
+
+
+
+
+
+
+
+
